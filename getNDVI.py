cb30e847aa6f145ded3969ca7200c57061b3570a
nickita <nickita.seomin@yandex.ru>
2024-01-17 22:30:41 +0300

    add server django rest

--- getNDVI.py
+++ getNDVI.py
@@ -54,49 +54,54 @@ def NDVI(r: np.array, n: np.array) -> np.array:
 
 if __name__ == "__main__":
     # Parse command line arguments.
-    if len(sys.argv) == 1:
-        prog = os.path.basename(sys.argv[0])
-        print('      ' + sys.argv[0] + ' [options]')
-        print("For more information try: python3 ", prog, " --help")
-        print("or: python3 ", prog, " -h")
-        print("example python3  {} --red_band red.tif --nir_band nir.tif --path .".format(sys.argv[0]))
-        print(
-            "example python3  {} --red_band red.tif --nir_band nir.tif --path . --output ndvi.tif".format(sys.argv[0]))
-        sys.exit(-1)
-    else:
-        usage = "usage: %prog [options] "
-        parser = OptionParser(usage=usage)
-        parser.add_option("-r", "--red_band", dest="red", action="store", type="string",
-                          help="Name of the red band image.", default=None)
-        parser.add_option("-n", "--nir_band", dest="nir", action="store", type="string",
-                          help="Name of the nir band image.", default=None)
-        parser.add_option("-p", "--path", dest="path", action="store", type="string", help="Path to data.",
-                          default=None)
-        parser.add_option("-o", "--output", dest="output", action="store", type="string",
-                          help="Name of the output image.", default=None)
-
-        (options, args) = parser.parse_args()
+    # if len(sys.argv) == 1:
+    #     prog = os.path.basename(sys.argv[0])
+    #     print('      ' + sys.argv[0] + ' [options]')
+    #     print("For more information try: python3 ", prog, " --help")
+    #     print("or: python3 ", prog, " -h")
+    #     print("example python3  {} --red_band red.tif --nir_band nir.tif --path .".format(sys.argv[0]))
+    #     print(
+    #         "example python3  {} --red_band red.tif --nir_band nir.tif --path . --output ndvi.tif".format(sys.argv[0]))
+    #     sys.exit(-1)
+    # else:
+    #     usage = "usage: %prog [options] "
+    #     parser = OptionParser(usage=usage)
+    #     parser.add_option("-r", "--red_band", dest="red", action="store", type="string",
+    #                       help="Name of the red band image.", default=None)
+    #     parser.add_option("-n", "--nir_band", dest="nir", action="store", type="string",
+    #                       help="Name of the nir band image.", default=None)
+    #     parser.add_option("-p", "--path", dest="path", action="store", type="string", help="Path to data.",
+    #                       default=None)
+    #     parser.add_option("-o", "--output", dest="output", action="store", type="string",
+    #                       help="Name of the output image.", default=None)
+    #
+    #     (options, args) = parser.parse_args()
 
         # Checking required arguments for the script.
-        parser.check_required("-r")
-        parser.check_required("-n")
-        parser.check_required("-p")
+        # parser.check_required("-r")
+        # parser.check_required("-n")
+        # parser.check_required("-p")
 
-        if options.output == None:
-            name = "NDVI.tiff"
-        else:
-            name = options.output
+        # if options.output == None:
+        name = "NDVI.tiff"
+        # else:
+        #     name = options.output
 
         z = zipfile.ZipFile('./dataFromSpace.zip')  # Flexibility with regard to zipfile
-        # for filename in z.namelist():
+        for c in z.namelist():
+            # print(c)
+            if c == 'B04_8.tiff':
+                red = rasterio.open('zip+file:./dataFromSpace.zip/' + c, "r")
+            else:
+                nir = rasterio.open('zip+file:./dataFromSpace.zip/' + c, "r")
 
         # Reading red band.
-        red = rasterio.open(os.path.join(options.path, options.red), "r")
+        # red = rasterio.open(os.path.join(options.path, options.red), "r")
         red_array = red.read()
         metadata = red.meta.copy()
 
         # Reading NIR band
-        nir = rasterio.open(os.path.join(options.path, options.nir), "r")
+        # nir = rasterio.open(os.path.join(options.path, options.nir), "r")
         nir_array = nir.read()
 
         # Calling the NDVI function.
@@ -106,7 +111,7 @@ if __name__ == "__main__":
         metadata.update({"driver": "GTiff", "dtype": rasterio.float32})
 
         # Writing the NDVI raster with the same properties as the original data
-        with rasterio.open(os.path.join(options.path, name), "w", **metadata) as dst:
+        with rasterio.open(os.path.join('.', name), "w", **metadata) as dst:
             if ndvi_array.ndim == 2:
                 dst.write(ndvi_array, 1)
             else:
--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-from PIL import Image
-import numpy
-
-im1 = Image.open('input/B08.tiff')
-im2 = Image.open('input/B04.tiff')
-# im.show()
-imarray1 = numpy.array(im1)
-imarray2 = numpy.array(im2)
-# print(imarray.shape)
-# print(im1.size)
-# print(imarray1)
-imRes1 = numpy.array([])
-# print(imarray1.size)
-# print(imarray1)
-# print()
-# print(imarray2)
-# print()
-# print(numpy.multiply(imarray1,imarray2))
-# print(imarray1[1])
-for i in range(len(imarray1)):
-    temp_list = (numpy.subtract(imarray1[i], imarray2[i]))
-    print(temp_list)
-    imRes1 = numpy.append(imRes1, temp_list, axis=0)
+import socket
+import sys
+s = socket.socket()
+s.bind(("localhost",8000))
+s.listen(10) # Acepta hasta 10 conexiones entrantes.
 
 
-print(imRes1)
-# imRes1.save("filename.tiff", format="TIFF", save_all=True)
-new_im = Image.fromarray(imRes1.astype('uint8'))
-new_im.show()
+while True:
+    sc, address = s.accept()
+
+
+    while (True):
+        # recibimos y escribimos en el fichero
+        l = sc.recv(1024)
+
+        if not l:
+            break
+
+    sc.close()
+    print('copied the file.')
+
+s.close()
